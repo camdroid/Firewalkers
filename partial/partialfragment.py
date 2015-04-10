f23e126e635bd3245605cf4a9a1b820174469b24
kubryant <ku.bryant@gmail.com>
2015-04-09 23:52:56 -0400

    ipv6 packets send

--- partial/partialfragment.py
+++ partial/partialfragment.py
@@ -6,27 +6,23 @@ This client sends fragmented packets with data on one side on the fragment!
 
 # EC2 Server IP
 # serverIP = "54.149.134.250"
-destIP = 'fe80::d328:22c0:2f25:3d69'
-# destIP = '::1'
+destIP = 'fe80::82e6:50ff:fe1b:93c2'
 srcIP = 'fe80::dead:beef'
 payload = 'hello'
 
 def constructPacket():
-    base = IPv6()
-    base.dst = destIP
-    base.src = srcIP
+    base = IPv6(src=srcIP, dst=destIP)
     extension = IPv6ExtHdrHopByHop()
 
-    #jumbo = Jumbo()
-    #jumbo.jumboplen = 2++30
-    #extension.options = jumbo
+    jumbo = Jumbo()
+    jumbo.jumboplen = 2++30
+    extension.options = jumbo
 
-    # return base/extension/payload
+    #packet = base/IPv6ExtHdrDestOpt()/IPv6ExtHdrRouting()/IPv6ExtHdrHopByHop()
+    return base/extension/payload
 
-    packet = base/IPv6ExtHdrDestOpt()/IPv6ExtHdrRouting()/IPv6ExtHdrHopByHop()
-    return packet
 
 if __name__ == '__main__':
     packet = constructPacket()
-    packet.show2()
+    #packet.show2()
     send(packet) 
