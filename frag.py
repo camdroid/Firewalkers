e7ca13621940e862feec4b3472ff94cc107e74ed
unknown <deboltj@umich.edu>
2015-04-11 22:58:16 -0400

    Frag n serve

--- frag.py
+++ frag.py
@@ -16,11 +16,20 @@ else:
 handshake=False
 
 
+if handshake:
+	ip=IPv6(src=sip, dst=dip)
+	SYN=TCP(sport=sport, dport=dport, flags="S", seq=100)
+	SYNACK=sr1(ip/SYN)
+
+	my_ack = SYNACK.seq + 1
+	ACK=TCP(sport=sport, dport=dport, flags="A", seq=101, ack=my_ack)
+	send(ip/ACK)
+
+else: my_ack=0
 
 
 
-
-payload="GET /?q=fuck HTTP/1.0\n\n"
+payload="GET /?q=vpn.*\xe5\x85\x8d\xe8\xb4\xb9\n\n"
 PUSH=TCP(sport=sport, dport=dport, flags="PA", seq=101, ack=my_ack)/payload
 fragpt=str(ip/PUSH)[40:]
 size=1
@@ -34,15 +43,11 @@ for off in range(0,len(fragpt),size*8):
 frags.append(ip/IPv6ExtHdrFragment(m=0, id=12343, 
 				nh=6, offset=len(fragpt)/8)/finpt)
 
+if not handshake:
+	for frag in frags:
+		frag.show()
+
 #send(ip/PUSH/payload)
 
 
 #handshake
-if handshake:
-	ip=IPv6(src=sip, dst=dip)
-	SYN=TCP(sport=sport, dport=dport, flags="S", seq=100)
-	SYNACK=sr1(ip/SYN)
-
-	my_ack = SYNACK.seq + 1
-	ACK=TCP(sport=sport, dport=dport, flags="A", seq=101, ack=my_ack)
-	send(ip/ACK)
